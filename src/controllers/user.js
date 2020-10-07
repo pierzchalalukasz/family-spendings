546b7ccc9b2f298f37e5ad9e2785b9e39ed41795
remedylove <lukaasz.pierzchala@gmail.com>
2020-10-07 22:39:49 +0200

    Extended addUser method - checking if given email is not in use yet, hashing password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export const getById = ({ UserModel }) => async (req, res) => {
   const user = await UserModel.findById(req.params.id);
 
@@ -9,9 +11,28 @@ export const getById = ({ UserModel }) => async (req, res) => {
 };
 
 export const addUser = ({ UserModel }) => async (req, res) => {
-  const user = await UserModel.create(req.body);
+  const { name, email, password } = req.body;
 
-  res.send(user);
+  try {
+    //  Checking if user with given email is not already in db
+    const userAlreadyExists = await UserModel.find({ email });
+    if (userAlreadyExists) {
+      throw Error('User with given email already exists.');
+    }
+    // Creating salt to hash password
+    const salt = await bcrypt.genSalt(10);
+    // Hashing password
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //  Creating a new user
+    const user = await UserModel.create(email, name, hashedPassword);
+    if (!user) {
+      throw Error('An error occured when creating a new user.');
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).json({ errorMsg: error.message });
+  }
 };
 
 export const getAll = ({ UserModel }) => async (req, res) => {
