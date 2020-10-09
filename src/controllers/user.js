904e6187e480915b893f30d6c05a79eb33647c26
remedylove <lukaasz.pierzchala@gmail.com>
2020-10-09 10:16:42 +0200

    Got rid of ternary operators in user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,24 +1,33 @@
 export const getById = ({ UserService }) => async (req, res) => {
-  UserService.getById(req.params.id)
-    .then((user) => (user ? res.json(user) : res.sendStatus(404)));
-  // .catch((err) => next(err));
+  const user = await UserService.getById(req.params.id);
+  if (!user) {
+    return res.send(404);
+  }
+  return res.json(user);
 };
 
 export const authenticateUser = ({ UserService }) => async (req, res) => {
   const { email, password } = req.body;
-  UserService.authenticateUser(email, password)
-    .then((user) => (user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect.' })));
-  // .catch((err) => next(err));
+  const { user, token } = await UserService.authenticateUser(email, password);
+
+  if (!user) {
+    return res.send(404);
+  }
+  return res.json({ user, token });
 };
 
 export const addUser = ({ UserService }) => async (req, res) => {
-  UserService.addUser(req.body)
-    .then((user) => (user ? res.json(user) : res.status(409).json({ message: 'The user with given email already exists.' })));
-  // .catch((err) => next(err));
+  const user = await UserService.addUser(req.body);
+  if (!user) {
+    return res.send(409);
+  }
+  return res.json(user);
 };
 
 export const getAll = ({ UserService }) => async (req, res) => {
-  UserService.getAll()
-    .then((users) => res.json(users));
-  // .catch(((err) => next(err)));
+  const users = await UserService.getAll();
+  if (!users) {
+    return res.send(404);
+  }
+  return res.json(users);
 };
